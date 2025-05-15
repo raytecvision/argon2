a601c508cfeb311678ff45886eb8159ae0b71a8e
GOMA2 <goma2@raytecvision.com>
2025-05-15 15:44:47 +0200

    Add install.sh script.

--- install.sh
+++ install.sh
@@ -21,8 +21,5 @@ echo ${project_name}
 build_path=build-${project_name}-Desktop-${make_tag}
 
 cd ..
-mkdir -p ${build_path}
 cd ${build_path}
-/usr/bin/cmake ../argon2 '-GCodeBlocks - Unix Makefiles' -DCMAKE_BUILD_TYPE:STRING=${make_tag} -DCMAKE_CXX_COMPILER:STRING=/usr/bin/g++ -DCMAKE_C_COMPILER:STRING=/usr/bin/gcc -DCMAKE_PREFIX_PATH:STRING=/usr -DQT_QMAKE_EXECUTABLE:STRING=/usr/lib/qt5/bin/qmake
-make clean
-make -j8
+make install
